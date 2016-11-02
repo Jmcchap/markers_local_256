985702cd206d860901dc8de2fd8d5346ef74249c
Jmcchap <Jmcchap@gmail.com>
2016-11-02 11:24:23 -0500

    I don't understand git
    IR version that was reverted, just placed in the origonal file.

--- markers_local_256.ino
+++ markers_local_256.ino
@@ -1,12 +1,16 @@
+//IR version of markers local
 
+#include <QTRSensors.h>
 #include <Time.h>
 #include <TimeLib.h>
 
-    // Inputs. Hall sensors that go to LOW when a marker is removed
-  const int marker_A1 = 4;   //D1, Door-side Left-most marker slot
-  const int marker_A2 = 5;   //D2, Door-side Right-most marker slot
-  const int marker_B1 = 6;   //D3, Fab-side Left-most marker slot
-  const int marker_B2 = 7;   //D4, Fab-side Right-most marker slot
+    // Inputs 
+    
+  QTRSensorsAnalog qtra((unsigned char[] {5, 6, 7, 8}, 4)  //pin locations of the IR sensors
+  
+  int sensor_values[4] = {0, 0, 0, 0};
+  
+
   
   
     //Outputs. Buzzers that buzz when the markers have been 'checked out' too long. 
@@ -16,16 +20,35 @@
   const int buzzerB_tone = 2000;    //Frequency buzzer B tones
 
    //Assorted variables used
-   int        checkout_time = 3600000;  //how long the marker can be removed before the buzzer goes off (1hr)
-   time_t     countdown_time[4] = {0, 0, 0, 0};       //how much time has elapsed since each marker was pulled
-     
+   int      alloted_time = 3600000;                       //how long the marker can be removed before the buzzer goes off (1hr)
+   int      checkout_time[4] = {0, 0, 0, 0};             //how much time the user is allowed to borrow the marker
+   int      light_threashold[4] = {511, 511, 511, 511};   //to fine tune later. The value the sensors return that mean a maker is removed. 
+   time_t   current_time = 0; 
 
 void setup() {
   // put your setup code here, to run once:
-  time_t     current_time = now();        //get the current time (seconds, since Jan 1 1970)
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  current_time = now();
+   read(sensor_values, QTR_EMITTERS_ON);
+
+   for (i=0; i<4; i++){
+    if(light_threashold[i] < snesor_value[i]){
+      checkout_time[i] = current_time + alloted_time;
+    }
+    if(checkout_time[i] > current_time){
+      if(i<2){
+        TONE A;
+      }
+      else(){
+        TONE B;
+        i = 0;
+      }
+     }else{
+      i = 0;
+     }
+   }
 }
