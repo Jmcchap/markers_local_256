af05edc83d81d48d0c606f9a8b92347a6c6d7ec3
Jessie <Jmcchap@gmail.com>
2017-01-05 00:52:08 -0600

    Buzzer Timer Change
    * Changed buzzer going off from 1 hour to 5 minutes

--- markers_local_256.ino
+++ markers_local_256.ino
@@ -20,11 +20,11 @@
   const int buzzerB = 9;            //D6, Fab-side buzzer
   const int buzzerA_tone = 1000;    //Frequency buzzer A tones
   const int buzzerB_tone = 2000;    //Frequency buzzer B tones
-   unsigned long buzzerB_duration = 1000;  //Buzzer B length of one tone
-   unsigned long buzzerA_duration = 1000;  //Buzzer A length of one tone
+  unsigned long buzzerB_duration = 1000;  //Buzzer B length of one tone
+  unsigned long buzzerA_duration = 1000;  //Buzzer A length of one tone
 
    //Assorted variables used
-   int      alloted_time = 3600000;                       //how long the marker can be removed before the buzzer goes off (1hr)
+   int      alloted_time = 300000;                       //how long the marker can be removed before the buzzer goes off (5 minutes)
    int      checkout_time[4] = {0, 0, 0, 0};             //how much time the user is has been borrowing the marker
    int      light_threashold[4] = {511, 511, 511, 511};   //to fine tune later. The value the sensors return that mean a maker is removed. 
    time_t   current_time = 0; 
