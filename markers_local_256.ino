6a9445684ee4ff03253f78100e08c1036a615df6
Jmcchap <Jmcchap@gmail.com>
2017-03-07 17:20:11 -0600

    Working on Eradicating Error Messages
    Mostly updating so I can work on this at the shop later. Just trying to
    get rid of all the error messages before I work on the WiFi/MQTT stuff

--- markers_local_256.ino
+++ markers_local_256.ino
@@ -1,4 +1,4 @@
-#include <dummy.h>
+
 
 #include <ESP8266WiFi.h>
 //#include <WifiManager.h>
@@ -12,7 +12,7 @@ int pins[] = {D5, D6, D7, D8};              //Digital pins
 /* OUTPUTS */
 
 /*CONSTANTS*/
-int allotedTime = 5000;                          //How much time the user is allowed to check out the pen (5 seconds)
+int allotedTime = 5000;                        //How much time the user is allowed to check out the pen (5 seconds)
 int lightThreshold[] = {511, 511, 511, 511};   //The value in which the pen is considered removed. NEEDS TRIAL
 
 
@@ -26,78 +26,79 @@ bool overdue[] = {false, false, false, false};  //Pretty much determines if the 
 
 
 //connect to WiFi network
-void setup_wifi(){
+//void setup_wifi() {
+//
+//  Serial.println("Starting wireless.");
+//  WiFiManager wifiManager; //Load the Wi-Fi Manager library.
+//  wifiManager.setTimeout(300); //Give up with the AP if no users gives us configuration in this many secs.
+//  if (!wifiManager.autoConnect()) {
+//    Serial.println("failed to connect and hit timeout");
+//    delay(3000);
+//    ESP.restart();
+//  }
+//
+//  Serial.println("");
+//  Serial.print("Successfully connected to ");
+//  Serial.println(WiFi.localIP());
+//}
 
-  Serial.println("Starting wireless.");
-  WiFiManager wifiManager; //Load the Wi-Fi Manager library.
-  wifiManager.setTimeout(300); //Give up with the AP if no users gives us configuration in this many secs.
-  if(!wifiManager.autoConnect()) {
-    Serial.println("failed to connect and hit timeout");
-    delay(3000);
-    ESP.restart();
-  }
-  
-  Serial.println("");
-  Serial.print("Successfully connected to ");
-  Serial.println(WiFi.localIP());
-}
-
-void setup() {
-  setup_wifi();
- 
-  for(i=0; i < sensorQTY; i++){
-    pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
-  }
-}
+//void setup() {
+//  setup_wifi();
+//
+//  for (i = 0; i < sensorQTY; i++) {
+//    pinMode(i, OUTPUT);
+//    digitalWrite(i, LOW);
+//  }
+//}
 
 /*read the sensor that is highlighted (sensorIndex) by setting the Digital output to high for the sensor
-and reading it from the Analog sensor A0.*/
-readSensors(sensorIndex){
+  and reading it from the Analog sensor A0.*/
+void readSensors(int sensorIndex){
   digitalWrite(sensorIndex, HIGH);
   delay(250);
   sensorValue[sensorIndex] = analogRead(A0);
-  digitalWrite(sensorIndex, LOW);
 }
 
-checkTimer(sensorIndex){
-  currentTime = now();
-  if(checkoutTime[sensorIndex] > currentTime){
-    overdue[sensorIndex] = TRUE;
+/*See if it is time for the marker to be returned*/
+void checkTimer(int sensorIndex){
+currentTime = now();
+  if (checkoutTime[sensorIndex] > currentTime) {
+    overdue[sensorIndex] = true;
   }
-}
 
-assignTimer(sensorIndex){
+
+/*Record the time that the marker should be returned*/
+void assignTimer(int sensorIndex) {
   checkoutTime[sensorIndex] = now() + allotedTime;
 }
 
 
 /*Compare the values of the sensor that is read against the light threashold that is set. If it is determined
-that the marker is removed, then a timer would be assigned*/
-compareSensor(sensorIndex){
-  if(sensorValue[sensorIndex] >= lightThreshold[sensorIndex]){
-    if(checkoutTime[sensorIndex] == 0);{
+  that the marker is removed, then a timer would be assigned*/
+void compareSensor(int sensorIndex) {
+  if (sensorValue[sensorIndex] >= lightThreshold[sensorIndex]) {
+    if (checkoutTime[sensorIndex] == 0); {
       assignTimer(sensorIndex);
     }
-    else{
+    else {
       checkTimer(sensorIndex);
     }
   }
 }
 
-buzzer(sensorIndex){
-  
-}
+//buzzer(sensorIndex) {
+//
+//}
 
 
 void loop() {
-  for(sensorIndex = 0; sensorIndex < sensorQTY; sensorIndex++){
+  for (sensorIndex = 0; sensorIndex < sensorQTY; sensorIndex++) {
     readSensors(sensorIndex);
     compareSensors(sensorIndex);
-    
-    if (overdue[sensorIndex] = true){
+
+    if (overdue[sensorIndex] = true) {
       buzzer(sensorIndex);
-      }
     }
+  }
 
 }
