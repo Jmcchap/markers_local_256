026662aa79dc502474797b6895c3a437e1b5b508
Jessie <Jmcchap@gmail.com>
2017-03-22 11:50:49 -0500

    Comments Added; markerDocked[] Added
    *This setup should let the marker be returned at any time.
    * I think there is something not quite lining up with sensorIndex and
    whatever actual sensor is being read. Currently doing Serial.print to
    figure it out.

--- Testing_Buildup/Testing_Buildup.ino
+++ Testing_Buildup/Testing_Buildup.ino
@@ -2,92 +2,113 @@
 #include <TimeLib.h>
 
 
-int pin[] ={D5, D6, D7, D8};
-int lightThreashold[] = {100, 100, 100, 100};
-int sensorValue[] = {0, 0, 0, 0};
-int checkoutTime[] = {0, 0, 0, 0};
-bool overdue[] {false, false, false, false};
-time_t currentTime = 0;
-int allotedTime = 50;
-int i = 0;
+int pin[] ={D5, D6, D7, D8};                    //digital output pins
+int lightThreashold[] = {1000, 100, 100, 100};   //determines if marker is 'in' or not
+int sensorValue[] = {0, 0, 0, 0};               //actual analog value from sensors
+int checkoutTime[] = {0, 0, 0, 0};              //how long each marker is 'allowed' to be removed
+bool overdue[] = {false, false, false, false};  //the flag for if the checkoutTime has surpassed how long it can be borrowed
+bool markerDocked[] = {true, true, true, true}; //if each marker is in its home, then true
+time_t currentTime = 0;                         //the time retrieved from the now() function
+int allotedTime = 50;                           //how long a marker can be withdrawn for
+int sensorIndex = 0;                            //which  sensor in the array
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
 pinMode(D5, OUTPUT);
 pinMode(D6, OUTPUT);
 pinMode(D7, OUTPUT);
 pinMode(D8, OUTPUT);
 pinMode(A0, INPUT);
+
 digitalWrite(D5, LOW);
 digitalWrite(D6, LOW);
 digitalWrite(D7, LOW);
 digitalWrite(D8, LOW);
+
 Serial.begin(9600);
 }
 
+/*readSensor sets one digital pin (sensorIndex) to HIGH and reads the analog value that the corresponding IR sensor has. 
+Then, it stores that value into the proper slot in sensorValue[] and returns the digital pin to LOW. */
 void readSensor(){
-  for( i=0; i<4; i++){
-         digitalWrite(pin[i], HIGH);
+  Serial.println("readSensor");
+        digitalWrite(pin[sensorIndex], HIGH);
         delay(250);
-        sensorValue[i] = analogRead(A0);  
-        digitalWrite(pin[i], LOW);
-        
-        delay(1000);
-        
-  }
-
-    Serial.print( "SensorValue[0] = ");
-        Serial.println(sensorValue[0]);
-        Serial.print( "SensorValue[1] = ");
-        Serial.println(sensorValue[1]);
-         Serial.print( "SensorValue[2] = ");
-        Serial.println(sensorValue[2]);
-         Serial.print( "SensorValue[3] = ");
-        Serial.println(sensorValue[3]);
+        sensorValue[sensorIndex] = analogRead(A0);  
+        digitalWrite(pin[sensorIndex], LOW);        
+        delay(250);
 }
 
+/*Find the time that the marker needs to be returned.*/
 void assignTimer() {
-  checkoutTime[i] = now() + allotedTime;
-  
+  Serial.print("assignTimer");
+  checkoutTime[sensorIndex] = now() + allotedTime;  
 }
-
-
-
 
 /*See if it is time for the marker to be returned*/
 void checkTimer(){
+  Serial.println("checkTimer");
 currentTime = now();
-  if (checkoutTime[i] < currentTime) {
-    overdue[i] = true;
-    
+  if (checkoutTime[sensorIndex] < currentTime) {
+    overdue[sensorIndex] = true;    
   }
 }
 
 void compareSensor(){
-  if(sensorValue[i] < lightThreashold[i]){
-     if (checkoutTime[i] == 0) {
-      assignTimer();
-    }
-    else {
-      checkTimer();
-    }    
+  Serial.println("compareSensor!");
+  Serial.print( "SensorValue[sensorIndex] = ");
+  Serial.println(sensorValue[sensorIndex]);
+  Serial.print("SensorValue[0] = ");
+  Serial.println(sensorValue[0]);
+
+  Serial.print("sensorIndex = ");
+  Serial.println(sensorIndex);
+  if(sensorValue[sensorIndex] < 1000){
+      markerDocked[sensorIndex] = false;
+  }else{
+      markerDocked[sensorIndex] = true;
   }
 }
 
 void loop() {
-  
-  readSensor();
-  compareSensor();
+  for( sensorIndex=0; sensorIndex<4; sensorIndex++){
+    readSensor();
+    compareSensor();    
+      if(markerDocked[sensorIndex] = false){      //if the marker is removed
+          if (checkoutTime[sensorIndex] == 0) {   //and no checkout time is assigned
+            assignTimer();                        //"checkout" the marker
+          }                                       //BUT
+          else {                                  //if the marker is already checked out
+            checkTimer();                         //check the timer to make sure it's not overdue
+          }
+      }else{                                      //if the marker is in its home
+           checkoutTime[sensorIndex] = 0;         //if so, reset the timer. It is returned
+      }
 
-//  Serial.print("overdue = ");
-//  Serial.println(overdue[0]);
-//  Serial.print("now = ");
-//  Serial.println(currentTime);
-//  Serial.print("checkoutTime[0] = ");
-//  Serial.println(checkoutTime[0]);
-//   Serial.print("checkoutTime[1] = ");
-//  Serial.println(checkoutTime[1]);
+     
+      
+  }
+  Serial.print( "SensorValue[0] = ");
+  Serial.println(sensorValue[0]);
+//  Serial.print( "SensorValue[1] = ");
+//  Serial.println(sensorValue[1]);
+//  Serial.print( "SensorValue[2] = ");
+//  Serial.println(sensorValue[2]);
+//  Serial.print( "SensorValue[3] = ");
+//  Serial.println(sensorValue[3]);
+  Serial.println("------------------------------------------");
+  Serial.print("markerDocked[0] = ");
+  Serial.println(markerDocked[0]);
+  Serial.print("now = ");
+  Serial.println(currentTime);
+  Serial.print("checkoutTime[0] = ");
+  Serial.println(checkoutTime[0]);
+  Serial.print("Overdue[0] = ");
+  Serial.println(overdue[0]);
+  
+
+
   
   Serial.println();
   Serial.println();
